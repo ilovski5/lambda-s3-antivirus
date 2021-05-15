109d02faed6f7b48d76cb99bd9b3945f43e695c1
Marjan <ilovski5@gmail.com>
2021-05-15 14:32:40 +0200

    node 12 support

--- build_lambda.sh
+++ build_lambda.sh
@@ -12,20 +12,13 @@ docker create -i -t -v ${PWD}/clamav:/home/docker  --name s3-antivirus-builder a
 docker start s3-antivirus-builder
 
 echo "-- Updating, downloading and unpacking clamAV and ClamAV update --"
-docker exec -it -w /home/docker s3-antivirus-builder yum install -y cpio yum-utils
 docker exec -it -w /home/docker s3-antivirus-builder yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-docker exec -it -w /home/docker s3-antivirus-builder yum-config-manager --enable epel
-docker exec -it -w /home/docker s3-antivirus-builder yumdownloader -x \*i686 --archlist=x86_64 clamav clamav-lib clamav-update json-c pcre2 libxml2 bzip2-libs libtool-ltdl xz-libs 
+docker exec -it -w /home/docker s3-antivirus-builder yum -y install clamav clamav-lib clamav-update json-c pcre2 libxml2 bzip2-libs libtool-ltdl xz-libs
 docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "echo 'folder content' && ls -la"
-docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "rpm2cpio clamav-0*.rpm | cpio -idmv"
-docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "rpm2cpio clamav-lib*.rpm | cpio -idmv"
-docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "rpm2cpio clamav-update*.rpm | cpio -idmv"
-docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "rpm2cpio json-c*.rpm | cpio -idmv"
-docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "rpm2cpio pcre2*.rpm | cpio -idmv"
-docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "rpm2cpio libxml2*.rpm | cpio -idmv"
-docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "rpm2cpio bzip2-libs*.rpm | cpio -idmv"
-docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "rpm2cpio libtool-ltdl*.rpm | cpio -idmv"
-docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "rpm2cpio xz-libs*.rpm | cpio -idmv"
+docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "ldd /usr/bin/clamscan | grep '=>' | grep -v -e '^$' | awk '{print \$3}' | xargs -I % sh -c 'cp %* .'"
+docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "ldd /usr/bin/freshclam | grep '=>' | grep -v -e '^$' | awk '{print \$3}' | xargs -I % sh -c 'cp %* .'"
+docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "cp /usr/bin/clamscan ."
+docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "cp /usr/bin/freshclam ."
 
 docker stop s3-antivirus-builder
 docker rm s3-antivirus-builder
@@ -33,7 +26,7 @@ docker rm s3-antivirus-builder
 mkdir ./bin
 
 echo "-- Copying the executables and required libraries --"
-cp clamav/usr/bin/clamscan clamav/usr/bin/freshclam clamav/usr/lib64/* bin/.
+cp clamav/* bin/.
 
 echo "-- Cleaning up ClamAV folder --"
 rm -rf clamav
